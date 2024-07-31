6b128037ccb0e22607c7ecde739bf3fd0300fe14
Cecilia <ravacecilia1@gmail.com>
2024-07-31 14:34:32 -0300

    feat: Add a click event to each link (argentina, canada, germany, unitedStates)

--- script.js
+++ script.js
@@ -6,3 +6,8 @@ let elementTitle = document.getElementById('title');
 let elementDescription = document.getElementById('description');
 let elementSubtitle = document.getElementById('subtitle');
 let elementParagraph = document.getElementById('paragraph');
+
+// Add a click event to each link
+links.forEach(function(link){
+    link.addEventListener('click');
+})
