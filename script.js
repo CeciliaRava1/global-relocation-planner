3dd6fd28e53084a0184a97e5baa1e9f0297f1b11
Cecilia <ravacecilia1@gmail.com>
2024-07-31 14:28:05 -0300

    feat: Obtain elements from the DOM

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 import { argentina, canada, germany, unitedStates } from './countries.js';
+
+// Obtain elements from the DOM
+let links = document.querySelectorAll('a');
+let elementTitle = document.getElementById('title');
+let elementDescription = document.getElementById('description');
+let elementSubtitle = document.getElementById('subtitle');
+let elementParagraph = document.getElementById('paragraph');
